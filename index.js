e0ed383f21d2717a2d98153d66846b476dd8a5b0
zhfrnn <zhafran.ph@gmail.com>
2022-09-03 13:38:49 +0700

    frist commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Jadi ini hanya untuk daily in my life hahaha");
+
+console.log("aku masi bingung mau belajar flutter apa engga soalnya kalo di lihat lihat kalo pake flutter gampang sih jadi pengen aja gitu.");
