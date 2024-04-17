772482f91c5289edd6d0e2c3c803a34dd7962f30
zhfrnn <zhafran.ph@gmail.com>
2024-04-17 19:27:20 +0700

    Menambah akhir cerita bukan akhir kisah.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log("Jadi ini hanya untuk daily in my life hahaha");
 
 console.log("aku masi bingung mau belajar flutter apa engga soalnya kalo di lihat lihat kalo pake flutter gampang sih jadi pengen aja gitu.");
 
-console.loh("JANGAN PERNAH PERCAYA KATA 'aku ga bakalan ninggalin kamu', carilah wanita yang berkata 'kita perbaiki ini, aku gamau udahan' jangan 'gatau, terserah kamu' ")
+console.log("Hey, 'K' you such a pretty girl ")
